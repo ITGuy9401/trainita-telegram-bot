02f27fb7afaa49dbea5f51c3feafbbe6d531f4e6
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-06-07 13:53:00 +0200

    test

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const Telegraf = require('telegraf');
 const app = new Telegraf(process.env.BOT_TOKEN);
 app.command('start', ({from, reply}) => {
     console.log('start', from);
-    return reply('Welcome!');
+    let language = from.language_code.split('-')[0];
+
+    if (language.toLowerCase() === 'it') {
+        return reply('Ciao!');
+    } else {
+        return reply('Welcome!');
+    }
+
 });
 app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+app.hears('CIAONE', (ctx) => ctx.reply('PRONTONE'));
 app.on('sticker', (ctx) => ctx.reply('👍'));
 app.startPolling();
