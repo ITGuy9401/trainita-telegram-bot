d2a5abb551a3b0b3a2a2ee6d597d767b5bb308f9
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-06-07 22:23:22 +0200

    service definition

--- translation-service.js
+++ translation-service.js
@@ -1,1 +1,36 @@
-const dao = require('.\database');
+const {mapping} = require('.\database');
+
+/**
+ *
+ * resolves a key with given arguments in the selected language
+ *
+ * @param language ISO (2 char) language code
+ * @param key key to be resolved
+ * @param args arguments to be resolved in key
+ */
+function resolveKey(language, key, args) {
+
+}
+
+/**
+ *
+ * get a list of every key with it's value in the form of:
+ *
+ * <code>
+ *     {
+ *      'key1': 'value1',
+ *      'key2': 'value2 {0]'
+ *     }
+ * </code>
+ *
+ * @param language ISO (2 char) language code
+ * @param bundle (OPTIONAL) bundle code for filtering
+ */
+function getAllKeys(language, bundle) {
+
+}
+
+module.exports = {
+    'resolveKey': resolveKey,
+    'getAllKeys': getAllKeys
+};
